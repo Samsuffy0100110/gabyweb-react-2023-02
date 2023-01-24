3fcdcc916c452d96c3ebbb073282a1d11fc60cb1
samSuffy <yobrambilla@gmail.com>
2023-01-24 09:46:53 +0100

    add project in DB

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,21 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
+CREATE TABLE `user` (
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `pseudo` VARCHAR(100) NULL,
+  `password` VARCHAR(255) NOT NULL
+) ENGINE=InnoDB;
+
+INSERT INTO `user` (`pseudo`, `password`) VALUES ('admin', 'adminpass');
+
+CREATE TABLE project (
+  `id` int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `date` datetime NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `stack` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+INSERT INTO project (`title`, `description`, `date`, `image`, `stack`, `link`) VALUES 
+('Pep\'s Design', 'Pep\'s Design est un projet de site de vente en ligne.', '2022-12-31 00:00:00', 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg', 'Symfony, Bootstrap, Twig, PHP, MySQL, HTML, SCSS, JavaScript', 'https://www.pepsdesign.fr');
+
---
+++ backend/src/models/ProjectManager.js
@@ -0,0 +1,36 @@
+const AbstractManager = require("./AbstractManager");
+
+class ProjectManager extends AbstractManager {
+  constructor() {
+    super({ table: "project" });
+  }
+
+  insert(project) {
+    return this.connection.query(
+      `insert into ${this.table} (title, description, date, image, stack, link) values (?, ?, ?, ?, ?, ?, ?)`,
+      [project.title],
+      [project.description],
+      [project.date],
+      [project.image],
+      [project.stack],
+      [project.link]
+    );
+  }
+
+  update(project) {
+    return this.connection.query(
+      `update ${this.table} set title = ?, description = ?, date = ?, image = ?, stack = ?, link = ? where id = ?`,
+      [
+        project.title,
+        project.description,
+        project.date,
+        project.image,
+        project.stack,
+        project.link,
+        project.id,
+      ]
+    );
+  }
+}
+
+module.exports = ProjectManager;
