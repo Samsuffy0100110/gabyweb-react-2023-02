535c0505b7a6fc23ef10e69c0e394ff9e173ab5d
samSuffy <yobrambilla@gmail.com>
2023-01-24 07:36:16 +0100

    #3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import Nav from "./components/layouts/Nav";
 import Footer from "./components/layouts/Footer";
+import Admin from "./pages/Admin";
 
 import "./App.scss";
 
 function App() {
   return (
     <div>
-      <Nav />
-      <Home />
-      <Footer />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- frontend/src/components/layouts/Nav.jsx
+++ frontend/src/components/layouts/Nav.jsx
@@ -1,14 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React from 'react';
+import { Link } from "react-router-dom";
+
 export default function Nav() {
   return (
     <nav className="nav">
-      Nav
-      {/* <Link to="/admin">Admin</Link> */}
-      {/* <Link to="/">Home</Link> */}
-      <Router>
-        <Link to="/legals">Admin</Link>
-        </Router>
+        <Link to="/">Home</Link>
+        <Link to="/admin">Admin</Link>
     </nav>
   );
 }
