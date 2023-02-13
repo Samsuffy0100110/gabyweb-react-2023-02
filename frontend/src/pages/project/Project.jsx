76c6e682a71be9bf2053a15849dc3e640efe454f
samSuffy <yobrambilla@gmail.com>
2023-02-13 10:22:52 +0100

    styles on project

--- frontend/src/pages/project/Project.jsx
+++ frontend/src/pages/project/Project.jsx
@@ -1,11 +1,11 @@
 import { Footer } from "@components/layouts/footer/Footer";
-import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 import { Nav } from "@components/layouts/nav/Nav";
 import style from "./project.module.scss";
 
 export function Project() {
-    const [project, setProject] = useState('');
+    const [project, setProject] = useState("");
     const [stacks, setStacks] = useState([]);
     const { id } = useParams();
     const uploadPath = "/project/";
@@ -15,8 +15,8 @@ export function Project() {
     useEffect(() => {
         try {
             fetch(`${baseURL}/project/${id}`)
-                .then(res => res.json())
-                .then(data => setProject(data))
+                .then((res) => res.json())
+                .then((data) => setProject(data));
         } catch (error) {
             console.log(error);
         }
@@ -47,20 +47,49 @@ export function Project() {
         return dateUTCArrayFinal;
     };
 
+    // return (
+    //     <main className={style.container}>
+    //         <Nav />
+    //         <h1>{project.name}</h1>
+    //         <p>{project.description}</p>
+    //         <img src={imagePath + project.image} alt={project.name} width="200" />
+    //         <a href={project.url} target="_blank" rel="noopener noreferrer">{project.url}</a>
+    //         <p>{formatDate(project.date)}</p>
+    //         {/* <h3>Stacks</h3>
+    //             {stacks.map(stack => (
+    //                     <img src={stack.image} alt={stack.name} width="50" />
+    //             ))} */}
+    //         <Link to="/">Retour</Link>
+    //         <Footer />
+    //     </main>
+    // )
+
     return (
-        <main className={style.container}>
+        <>
             <Nav />
-            <h1>{project.name}</h1>
-            <p>{project.description}</p>
-            <img src={imagePath + project.image} alt={project.name} width="200" />
-            <a href={project.url} target="_blank" rel="noopener noreferrer">{project.url}</a>
-            <p>{formatDate(project.date)}</p>
-            {/* <h3>Stacks</h3>
-                {stacks.map(stack => (
-                        <img src={stack.image} alt={stack.name} width="50" />
-                ))} */}
-            <Link to="/">Retour</Link>
+            <main className={style.project__container}>
+                <div className={style.project__title}>
+                    <h1>{project.name}</h1>
+                </div>
+                <div className={style.project__description}>
+                    <p>{project.description}</p>
+                </div>
+                <div className={style.project__image}>
+                    <img src={imagePath + project.image} alt={project.name} />
+                </div>
+                <div className={style.project__url}>
+                    <a href={project.url} target="_blank" rel="noopener noreferrer">
+                        {project.url}
+                    </a>
+                </div>
+                <div className={style.project__date}>
+                    <p>{formatDate(project.date)}</p>
+                </div>
+                <div className={style.project__back}>
+                    <Link to="/">Retour</Link>
+                </div>
+            </main>
             <Footer />
-        </main>
-    )
+        </>
+    );
 }
