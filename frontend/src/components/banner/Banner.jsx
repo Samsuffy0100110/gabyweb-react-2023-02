f09a0eebf48b222056a009fe2ef86f94b86e4309
samSuffy <yobrambilla@gmail.com>
2023-02-14 13:50:28 +0100

    fix integration

--- frontend/src/components/banner/Banner.jsx
+++ frontend/src/components/banner/Banner.jsx
@@ -2,25 +2,23 @@ import style from "./banner.module.scss";
 
 export function Banner() {
   return (
-    <section className={style.container}>
-      <div className={style.banner}>
-        <div className={style.banner__content}>
-          <div className={style.title__content}>
-            <h1 className={style.title}>
-              GabyWeb
-              <br />
-              <span className={style.title__span}>Solution</span>
-            </h1>
-          </div>
-          <div className={style.subtitle__content}>
-            <h2 className={style.subtitle}>
-              Agence Web &
-              <br />
-              <span className={style.subtitle__span}>Marketing Digital</span>
-            </h2>
-          </div>
+    <div className={style.banner}>
+      <div className={style.banner__content}>
+        <div className={style.title__content}>
+          <h1 className={style.title}>
+            GabyWeb
+            <br />
+            <span className={style.title__span}>Solution</span>
+          </h1>
+        </div>
+        <div className={style.subtitle__content}>
+          <h2 className={style.subtitle}>
+            Agence Web &
+            <br />
+            <span className={style.subtitle__span}>Marketing Digital</span>
+          </h2>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
